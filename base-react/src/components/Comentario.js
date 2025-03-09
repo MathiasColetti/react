f5ed70e583e60bcd829ff0607a799eee1b197aca
Mathias Coletti <88839208+MathiasColetti@users.noreply.github.com>
2025-03-09 20:49:05 -0300

    Construido componentes

---
+++ base-react/src/components/Comentario.js
@@ -0,0 +1,10 @@
+import "./Comentario.css";
+const Comentario = () => {
+  return (
+    <div className="Comentario">
+      <p>João:</p>
+      <p>Olá, tudo bem</p>
+    </div>
+  );
+};
+export default Comentario;
