a6ca5e501a9c37b1684cab076a4c933378aec5a2
Mathias Coletti <88839208+MathiasColetti@users.noreply.github.com>
2025-03-09 21:14:17 -0300

    passando parametros para os componentes

--- base-react/src/App.js
+++ base-react/src/App.js
@@ -6,11 +6,51 @@ function App() {
   return (
     <div className="App">
       <h1>Meu Projeto</h1>
-      <Comentario />
-      <Comentario />
-      <Comentario />
+      <Comentario
+        name="Joao"
+        email="joao@gmail.com"
+        data={new Date(2025, 1, 2)}
+      >
+        Ola Tudo bem
+      </Comentario>
+      <Comentario
+        name="Maria"
+        email="maria@gmail.com"
+        data={new Date(2025, 2, 2)}
+      >
+        Ola Tudo bem sim, e com voce?
+      </Comentario>
     </div>
   );
 }
 
 export default App;
+
+// import React from "react";
+// import "./App.css";
+// import Comentario from "./components/Comentario";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Meu Projeto</h1>
+//       {/* Passando um objeto Date para props.data */}
+//       <Comentario
+//         name="João"
+//         email="joao@gmail.com"
+//         data={new Date(2025, 1, 2)} // Certifique-se de passar o valor de data corretamente
+//       />
+//       <Comentario />
+//       {/* Outro Comentario com data */}
+//       <Comentario
+//         name="Maria"
+//         email="maria@gmail.com"
+//         data={new Date(2025, 2, 2)} // Também passando o objeto Date
+//       >
+//         Olá, tudo bem sim, e com você?
+//       </Comentario>
+//     </div>
+//   );
+// }
+
+// export default App;
--- base-react/src/components/Comentario.js
+++ base-react/src/components/Comentario.js
@@ -1,10 +1,16 @@
-import "./Comentario.css";
-const Comentario = () => {
+const Comentario = (props) => {
+  // Verifique se props.data está definido antes de tentar chamar toString
+//   const dataString = props.data ? props.data.toString() : "Data não fornecida";
+
   return (
     <div className="Comentario">
-      <p>João:</p>
-      <p>Olá, tudo bem</p>
+      <p>{props.name}</p>
+      <p>{props.email}</p>
+      <p>{props.children}</p>
+      <p>{props.data.toString()}</p>
+      {/* Exibindo a data convertida ou mensagem padrão */}
     </div>
   );
 };
+
 export default Comentario;
