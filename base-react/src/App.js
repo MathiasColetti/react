4c7d6cb042b77a35661393f2d7da8ff7fa946782
Mathias Coletti <88839208+MathiasColetti@users.noreply.github.com>
2025-03-11 10:57:54 -0300

    Manipulando o Estado da aplicação

--- base-react/src/App.js
+++ base-react/src/App.js
@@ -1,57 +1,62 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 import Comentario from "./components/Comentario";
-import { Component } from "react";
 
 class App extends Component {
   state = {
     comentarios: [
       {
         id: 1,
-        name: "joao",
+        name: "João",
         email: "joao@gmail.com",
         data: new Date(2025, 1, 2),
-        mensagem: " Ola Tudo bem?",
+        mensagem: "Olá, tudo bem?",
       },
       {
         id: 2,
         name: "Maria",
         email: "maria@gmail.com",
         data: new Date(2025, 2, 2),
-        mensagem: " Ola Tudo bem sim, e com voce?",
+        mensagem: "Olá, tudo bem sim, e com você?",
       },
     ],
   };
-  adicionarComentario() {
-    console.log("Adicionando Comentario");
+
+  // Transformado em arrow function para manter o this correto
+  adicionarComentario = () => {
+    console.log("Adicionando Comentário");
+
     const novoComentario = {
+      id: this.state.comentarios.length + 1, // Garantindo ID único
       name: "Maria",
       email: "maria@gmail.com",
       data: new Date(),
-      mensagem: "Ola Passoal",
+      mensagem: "Olá, pessoal!",
     };
-    // this.state.comentarios.push(novoComentario); jeito errado
-    this.setState();
-  }
+
+    this.setState((prevState) => ({
+      comentarios: [...prevState.comentarios, novoComentario],
+    }));
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Meu Projeto</h1>
 
-        {this.state.comentarios.map((comentario, indice) => {
-          return (
-            <Comentario
-              key={comentario.id}
-              name={comentario.name}
-              email={comentario.email}
-              data={comentario.data}
-            >
-              {comentario.mensagem}
-            </Comentario>
-          );
-        })}
+        {this.state.comentarios.map((comentario) => (
+          <Comentario
+            key={comentario.id}
+            name={comentario.name}
+            email={comentario.email}
+            data={comentario.data}
+          >
+            {comentario.mensagem}
+          </Comentario>
+        ))}
+
         <button onClick={this.adicionarComentario}>
-          Adicionar um comentario
+          Adicionar um comentário
         </button>
       </div>
     );
